349d26977d21fd96d38f2ff8c0bcd3a86dfe3a4d
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-07-30 22:34:55 +0530

    added controller for sample model

---
+++ controllers/sample.controller.js
@@ -0,0 +1,82 @@
+const Sample = require("../models/sample.model");
+
+const getProducts = async (req, res) => {
+  try {
+    const products = await Sample.find();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+/**
+ * It's an asynchronous function that uses the Product model to find a product by its id, and then
+ * sends a response with the product's data.
+ * @param req - The request object.
+ * @param res - The response object.
+ */
+const getProduct = async (req, res) => {
+  try {
+    const product = await Sample.findById(req.params.id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+/**
+ * It creates a new product using the data from the request body and returns the created product in the
+ * response.
+ * @param req - The request object. This object represents the HTTP request and has properties for the
+ * request query string, parameters, body, HTTP headers, and so on.
+ * @param res - The response object.
+ */
+const createProduct = async (req, res) => {
+  try {
+    const product = await Sample.create(req.body);
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+/**
+ * It takes the id of the product to be updated from the request params, and the updated product data
+ * from the request body, and then updates the product in the database with the new data, and returns
+ * the updated product to the client.
+ * @param req - The request object.
+ * @param res - The response object.
+ */
+const updateProduct = async (req, res) => {
+  try {
+    const product = await Sample.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+/**
+ * It finds a product by its id and deletes it.
+ * @param req - The request object. This object represents the HTTP request and has properties for the
+ * request query string, parameters, body, HTTP headers, and so on.
+ * @param res - The response object.
+ */
+const deleteProduct = async (req, res) => {
+  try {
+    const product = await Sample.findByIdAndDelete(req.params.id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+module.exports = {
+  getProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+};
