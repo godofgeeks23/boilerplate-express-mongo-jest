d59ac25270ce064b2f50b15221e471a0fb7fe8d8
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-08-03 23:34:11 +0530

    Update sample.route.js

--- routes/sample.route.js
+++ routes/sample.route.js
@@ -13,7 +13,6 @@ const router = express.Router();
 /* Creating the routes for the product controller. */
 
 router.get("/products", getProducts);
-
 router.get("/products/:id", getProduct);
 
 router.post("/products", createProduct);
