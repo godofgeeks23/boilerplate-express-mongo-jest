73f3cfc40df1d02e8f4188fc534e5dc969402625
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-08-03 23:34:28 +0530

    Update sample.route.js

--- routes/sample.route.js
+++ routes/sample.route.js
@@ -16,7 +16,6 @@ router.get("/products", getProducts);
 router.get("/products/:id", getProduct);
 router.post("/products", createProduct);
 router.patch("/products/:id", updateProduct);
-
 router.delete("/products/:id", deleteProduct);
 
 module.exports = router;
